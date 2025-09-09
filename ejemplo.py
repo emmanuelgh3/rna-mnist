29e1226fa7adf29f2fc68a7728d27994e5198709
Emmanuel González <emmagh2122016@gmail.com>
2025-09-08 21:55:33 -0600

    ejemplo.py con momentum

--- ejemplo.py
+++ ejemplo.py
@@ -3,4 +3,5 @@ from mnist_loader import load_data_wrapper
 
 training_data, validation_data, test_data = load_data_wrapper()
 net = network.Network([784, 30, 10])  # arquitectura clásica del libro
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data, mu=0.7) #Agregamos mu
+
