d69077610d28758406e9419acbf75f840a6a2ab1
Emmanuel González <emmagh2122016@gmail.com>
2025-09-08 21:56:35 -0600

    ejemplo.py optimizado
    Se redujo el valor de lr y de mu para aumentar la eficiencia

--- ejemplo.py
+++ ejemplo.py
@@ -3,5 +3,6 @@ from mnist_loader import load_data_wrapper
 
 training_data, validation_data, test_data = load_data_wrapper()
 net = network.Network([784, 30, 10])  # arquitectura clásica del libro
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data, mu=0.7) #Agregamos mu
+net.SGD(training_data, 30, 10, 0.1, test_data=test_data, mu=0.4) 
+#lr y mu optimizados
 
